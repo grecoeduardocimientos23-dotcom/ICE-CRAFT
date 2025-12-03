6105070e7c1c70ea084bbe0ae4215f026cbd6760
grecoeduardocimientos23-dotcom <grecoeduardocimientos23@cbtis179.edu.mx>
2025-12-03 10:43:12 -0600

    Agregando archivos nuevos

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
+const comprasRoutes = require("./routes/compras");
 
+app.use("/compras", comprasRoutes);
 const app = express();
 
 // ======================================
